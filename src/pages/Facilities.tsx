4787e0fe0ed77ea6a806a8f4eb0fae1ceb88b07c
supunyasantha <supunyasantha.official@gmail.com>
2026-02-12 15:48:27 +0530

    Import Offers and Services Sections.

--- src/pages/Facilities.tsx
+++ src/pages/Facilities.tsx
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
-
+import Offers from "../components/Offers";
 import header3 from "../assets/images/Header3.jpg";
-
+import Services from "../components/Services"
 const Home = () => {
   return (
     <main>
@@ -10,12 +10,11 @@ const Home = () => {
         subtitle="Royal Mansion Resort,"
         description="We do not give you just rooms to stay. We give you an environment so you can expierence the best while on a holiday with us.Walk into our hotel and enjoy a refreshing, rejuvenating,day-off with us."
         bgImage={header3}
-        height="h-[70vh]"
+        height="h-[80vh]"
       />
 
-      {/* <section className="py-20 bg-white text-center">
-        <h2 className="text-3xl font-serif">Explore Our Story</h2>
-      </section> */}
+      <Offers />
+      <Services/>
     </main>
   );
 };
