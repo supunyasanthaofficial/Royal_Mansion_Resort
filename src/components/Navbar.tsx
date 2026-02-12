d2b8bb6c4a59493ed0ecd7e3d78d547f2fbfcdfa
supunyasantha <supunyasantha.official@gmail.com>
2026-02-12 08:49:04 +0530

    Add font-bold for pages title.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Logo from "../assets/images/Logo.png";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="w-full z-50 flex items-center px-10 py-5 bg-[#121212] text-white border-b border-white/10 relative">
+    <nav className="fixed top-0 left-0 w-full z-50 flex items-center px-10 py-5 bg-white/10 backdrop-blur-md text-white border-b border-white/10">
       <div className="flex items-center ml-12 z-10">
         <div className="w-12 h-12 flex items-center justify-center mr-4">
           <img
@@ -21,22 +21,31 @@ const Navbar: React.FC = () => {
 
       <ul className="absolute inset-0 flex items-center justify-center space-x-8 text-xs font-medium uppercase tracking-widest pointer-events-none">
         <li className="pointer-events-auto">
-          <Link to="/" className="hover:text-blue-400 transition">
+          <Link to="/" className="hover:text-blue-400 font-bold transition">
             Home
           </Link>
         </li>
         <li className="pointer-events-auto">
-          <Link to="/about" className="hover:text-blue-400 transition">
+          <Link
+            to="/about"
+            className="hover:text-blue-400 font-bold transition"
+          >
             About
           </Link>
         </li>
         <li className="pointer-events-auto">
-          <Link to="/facilities" className="hover:text-blue-400 transition">
+          <Link
+            to="/facilities"
+            className="hover:text-blue-400 font-bold transition"
+          >
             Facilities
           </Link>
         </li>
         <li className="pointer-events-auto">
-          <Link to="/contact" className="hover:text-blue-400 transition">
+          <Link
+            to="/contact"
+            className="hover:text-blue-400 font-bold transition"
+          >
             Contact
           </Link>
         </li>
